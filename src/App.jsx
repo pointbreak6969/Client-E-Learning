8153020fd9d074ea4905dd20742c39d6515399e4
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-05-20 22:57:37 +0545

    some minor changes in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,18 @@ import BecomeAInstructor from "./pages/BecomeAInstructor";
 import TestNavbar from "./components/TestNavbar";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path= "/register" Component = {Register}/>
-        <Route path="/login" Component={Login} />
-        <Route path="/about" Component={About} />
-        <Route path="/search" Component={SearchCourse} />
-        <Route path="/becomeInstructor" Component={BecomeAInstructor} />
-        <Route path="/testNavbar" Component={TestNavbar} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" Component={Home} />
+    //     <Route path= "/register" Component = {Register}/>
+    //     <Route path="/login" Component={Login} />
+    //     <Route path="/about" Component={About} />
+    //     <Route path="/search" Component={SearchCourse} />
+    //     <Route path="/becomeInstructor" Component={BecomeAInstructor} />
+    //     <Route path="/testNavbar" Component={TestNavbar} />
+    //   </Routes>
+    // </BrowserRouter>
+    <TestNavbar/>
   );
 }
 
--- src/components/TestNavbar.jsx
+++ src/components/TestNavbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "../assets/Logo.png";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { MdShoppingCart } from "react-icons/md";
-import { FaSearch } from "react-icons/fa";
+import { MdOutlineSearch } from "react-icons/md";
+import { BsCart3 } from "react-icons/bs";
 const TestNavbar = () => {
   return (
     <>
-      <div className="flex flex-nowrap justify-between p-3 shadow-md items-center sm:hidden ">
+      <div className="flex flex-nowrap justify-between p-3 shadow-md items-center md:hidden ">
         <div>
           <RxHamburgerMenu className="text-3xl" />
         </div>
@@ -15,11 +15,27 @@ const TestNavbar = () => {
         </div>
         <div className="flex">
           <div className="pr-2">
-            <FaSearch className="text-3xl"/>
+          <MdOutlineSearch className="text-3xl" />
           </div>
           <div className="pl-2">
-            <MdShoppingCart className="text-3xl"/>
+            <BsCart3 className="text-3xl"/>
           </div>
+
+        </div>
+      </div>
+      <div className="hidden md:grid md:grid-cols-12 p-5 ">
+        <div className="md:col-span-3">
+          <img src={logo} alt="" className="w-20" />
+        </div>
+        <div className="md:col-span-6 md:flex md:justify-center md:items-center ">
+          <input
+            type="text"
+            placeholder="Search for Courses"
+            className="w-full p-2 border-2 border-gray-300 rounded-md"
+          />
+        </div>
+        <div className="md:col-span-3 flex justify-end items-center">
+          <BsCart3 className="text-3xl"/>
         </div>
       </div>
     </>
