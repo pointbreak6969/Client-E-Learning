270cf3100851e28916755fb0166ff279200932e6
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-05-22 19:59:02 +0545

    chore: small change

--- src/components/TestNavbar.jsx
+++ src/components/TestNavbar.jsx
@@ -15,27 +15,38 @@ const TestNavbar = () => {
         </div>
         <div className="flex">
           <div className="pr-2">
-          <MdOutlineSearch className="text-3xl" />
+            <MdOutlineSearch className="text-3xl" />
           </div>
           <div className="pl-2">
-            <BsCart3 className="text-3xl"/>
+            <BsCart3 className="text-3xl" />
           </div>
-
         </div>
       </div>
       <div className="hidden md:grid md:grid-cols-12 p-5 ">
-        <div className="md:col-span-3">
-          <img src={logo} alt="" className="w-20" />
+        <div className="md:col-span-3 md:grid md:grid-cols-3 items-center">
+          <div>
+            <img src={logo} alt="" className="w-20" />
+          </div>
+          <div>
+            <p className = "text-lg">About Us</p>
+          </div>
+          <div>
+            <p className = "text-lg">Categories</p>
+          </div>
         </div>
         <div className="md:col-span-6 md:flex md:justify-center md:items-center ">
           <input
             type="text"
             placeholder="Search for Courses"
-            className="w-full p-2 border-2 border-gray-300 rounded-md"
+            className="w-full p-2 border-2 border-gray-300 rounded-lg"
           />
         </div>
-        <div className="md:col-span-3 flex justify-end items-center">
-          <BsCart3 className="text-3xl"/>
+        <div className="md:col-span-3 md:grid md:grid-cols-4 items-center pl-10 ">
+          <div><p className = "text-lg">Business</p></div>
+          <div><p className = "text-lg">Teacher</p></div>
+          <div><p className = "text-lg">Log in</p></div>
+          <div><p className = "text-lg">Sign up</p></div>
+          
         </div>
       </div>
     </>
