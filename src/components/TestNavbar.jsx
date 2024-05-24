11933a51a7d8f23feb93665ab987c326c523e9a4
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-05-24 21:57:49 +0545

    chore: Update TestNavbar component with responsive search bar and centered items

--- src/components/TestNavbar.jsx
+++ src/components/TestNavbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../assets/Logo.png";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdOutlineSearch } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
 import { BsCart3 } from "react-icons/bs";
 const TestNavbar = () => {
   return (
@@ -23,7 +24,7 @@ const TestNavbar = () => {
         </div>
       </div>
       <div className="hidden md:grid md:grid-cols-12 p-5 ">
-        <div className="md:col-span-3 md:grid md:grid-cols-3 items-center">
+        <div className="md:col-span-3 md:grid md:grid-cols-3 items-center justify-items-center">
           <div>
             <img src={logo} alt="" className="w-20" />
           </div>
@@ -34,14 +35,15 @@ const TestNavbar = () => {
             <p className = "text-lg">Categories</p>
           </div>
         </div>
-        <div className="md:col-span-6 md:flex md:justify-center md:items-center ">
+        <div className="md:col-span-6 md:flex md:justify-center md:items-center md:gap-5 md:border-2 md:rounded-3xl ">
+        <IoIosSearch className="text-3xl  ml-5 opacity-50"/>
           <input
             type="text"
             placeholder="Search for Courses"
-            className="w-full p-2 border-2 border-gray-300 rounded-lg"
+            className="w-full   border-gray-300 rounded-lg border-0 focus:outline-none focus:border-none placeholder:text-lg "
           />
         </div>
-        <div className="md:col-span-3 md:grid md:grid-cols-4 items-center pl-10 ">
+        <div className="md:col-span-3 md:grid md:grid-cols-4 items-center justify-items-center ">
           <div><p className = "text-lg">Business</p></div>
           <div><p className = "text-lg">Teacher</p></div>
           <div><p className = "text-lg">Log in</p></div>
